c2f8fe67e6b0ad67f0824a5426e46470387e291d
greetesiemann <greete.siemann@gmail.com>
2024-11-13 16:05:13 +0200

    kommentaarid ja uued muutuja nimed

--- esialge_kood.py
+++ esialge_kood.py
@@ -8,12 +8,12 @@
 #
 # Autorid: Hanna Samelselg, Greete Siemann
 #
-# mõningane eeskuju:
 #
 # Lisakommentaar (nt käivitusjuhend):
 #
 ##################################################
 
+#Funktsioon, mis väljastab sobiliku retsepti, lugedes andmed failist.
 def väljasta_retsept(faili_nimi):
     fail = open(faili_nimi, encoding = "utf-8")
     for rida in fail:
@@ -21,54 +21,65 @@ def väljasta_retsept(faili_nimi):
     fail.close()
 
 
-def kas_saab_valmistada(retseptid, toiduained):
+#Funktsioon, mis kontrollib, kas toitu saab valmistada ja seejärel lisab järjendisse.
+def kas_saab_valmistada(retsepti_kogu, sisestatud_toiduained):
     saab_valmistada = []
-    for retsept in retspetid:
+    for retsept in retsepti_kogu:
         luger = 0
         for toiduaine in retsept:
-            if toiduaine in toiduained:
+            if toiduaine in sisestatud_toiduained:
                 luger += 1
         if luger == len(retsept):
             saab_valmistada.append(retsept)
     return saab_valmistada
 
 
-def pasta(järjend):
-    if kana_pasta in järjend:
+#Järgnevad funktsioonid kontrollivad roa olemasolu saab_valmistada järjendis ning vajadusel kutsuvad välja funktsiooni, mis väljastab retsepti.
+def kas_saab_teha_kanapasta(võimalikud_retseptid):
+    if kanapasta in võimalikud_retseptid:
         pasta_fail = "kanapasta.txt"
+        print("\nHakkad valmistama kanapastat")
         väljasta_retsept(pasta_fail)
 
-def frikadelli_supp(järjend):
-    if frikadelli_supp in järjend:
+
+def kas_saab_teha_frikadellisupp(võimalikud_retseptid):
+    if frikadellisupp in võimalikud_retseptid:
         frikadellisupp_fail = "frikadellisupp.txt"
+        print("\nHakkad valmistama frikadellisuppi")
         väljasta_retsept(frikadellisupp_fail)
 
-def lihapallid(järjend):
-    if lihapallid in järjend:
+
+def kas_saab_teha_lihapallid(võimalikud_retseptid):
+    if lihapallid in võimalikud_retseptid:
         lihapallid_fail = "lihapallid.txt"
+        print("\nHakkad valmistama lihapalle")
         väljasta_retsept(lihapallid_fail)
 
-def kanašnitsel(järjend):
-    if kanašnitsel in järjend:
+
+def kas_saab_teha_kanašnitsel(võimalikud_retseptid):
+    if kanašnitsel in võimalikud_retseptid:
         kanašnitsel_fail = "kanašnitsel.txt"
+        print("\nHakkad valmistama kanašnitslit")
         väljasta_retsept(kanašnitsel_fail)
 
 
-kana_pasta = ["kana", "pasta", "piim", "merevaik"]
-frikadelli_supp = ["kartul", "porgand", "frikadellid", "puljong"]
+
+kanapasta = ["kana", "pasta", "piim", "merevaik"]
+frikadellisupp = ["kartul", "porgand", "frikadellid", "puljong"]
 lihapallid = ["hakkliha", "muna", "riivsai", "sibul", "küüslauk"]
 kanašnitsel = ["kana", "muna", "jahu", "riivsai"]
-retspetid = [kana_pasta, frikadelli_supp, lihapallid, kanašnitsel]
+retspetid = [kanapasta, frikadellisupp, lihapallid, kanašnitsel]
 
 
+#Põhifunktsioon
 def main():
     toiduained = str(input("Sisesta olemasolevad toiduained: "))
     toiduained = set(toiduained.split(", "))
     saab_teha = kas_saab_valmistada(retspetid, toiduained)
-    pasta(saab_teha)
-    frikadelli_supp(saab_teha)
-    lihapallid(saab_teha)
-    kanašnitsel(saab_teha)
+    kas_saab_teha_kanapasta(saab_teha)
+    kas_saab_teha_frikadellisupp(saab_teha)
+    kas_saab_teha_lihapallid(saab_teha)
+    kas_saab_teha_kanašnitsel(saab_teha)
 
 
 if __name__ == "__main__":
